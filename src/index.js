700ef7f7b64b99fbf2bf4cb5d7ab44622ffb692d
John Curry <fastpenguin91@gmail.com>
2021-09-05 21:30:23 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const { ApolloServer } = require("apollo-server-express");
+const http = require("http");
+const express = require("express");
+
+const app = express();
+
+const httpServer = http.createServer(app);
+
+const typeDefs = `
+type Query {
+    helloWorld: String!
+}
+`;
+
+const resolvers = {
+  Query: {
+    helloWorld: () => `Hi there dude!`,
+  },
+};
+
+const corsOptions = {
+  credentials: true,
+  origin: [process.env.ORIGIN, "https://studio.apollographql.com"],
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const startServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app, cors: corsOptions });
+  await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+  console.log(`Server ready at http://localhost:4000`);
+};
+
+startServer();
