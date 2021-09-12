47f67b97b45e19999d77c19355fe06d81b4e6ad7
John Curry <fastpenguin91@gmail.com>
2021-09-12 12:02:20 -0700

    typedefs pulled

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ const passport = require("passport");
 const { GraphQLLocalStrategy, buildContext } = require("graphql-passport");
 const session = require("express-session");
 const SESSION_SECRET = "asdlfkwheifahoalkhj12hahiw";
+const { typeDefs } = require("./typeDefs");
 
 passport.serializeUser((user, done) => {
   done(null, user.id); // what you want to pass into the session (you could pass entire user if you wanted.
@@ -72,51 +73,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 const httpServer = http.createServer(app);
-
-const typeDefs = `
-type Query {
-    helloWorld: String!
-    users(text: String): [User!]!
-    todos(filter: String, takeStatus: String, skip: Int, take: Int): Todos!
-    me: User
-}
-type Mutation {
-  login(email: String!, password: String!): User
-  logout: Boolean
-  signup(firstName: String!, email: String!, password: String!, age: Int): User
-  createTodo(name: String!, isComplete: Boolean!, userId: ID!): Todo
-  deleteTodo(todoId: ID!): Todo
-  updateTodo(todoId: ID!, name: String, isComplete: Boolean): Todo
-  deleteTodos(todoIds: [ID!]): BatchPayload!
-  resetTodos: BatchPayload!
-}
-
-type User {
-  id: ID!
-  firstName: String!
-  email: String!
-  age: Int
-  todos(text: String): [Todo!]!
-}
-
-type Todo {
-  id: ID!
-  name: String!
-  isComplete: Boolean!
-  user: User!
-  userId: ID!
-}
-
-type Todos {
-  count: Int!
-  todoItems: [Todo!]
-}
-
-type BatchPayload {
-  count: Int!
-}
-
-`;
 
 const resolvers = {
   Query: {
---
+++ src/typeDefs.js
@@ -0,0 +1,48 @@
+const typeDefs = `
+type Query {
+    helloWorld: String!
+    users(text: String): [User!]!
+    todos(filter: String, takeStatus: String, skip: Int, take: Int): Todos!
+    me: User
+}
+type Mutation {
+  login(email: String!, password: String!): User
+  logout: Boolean
+  signup(firstName: String!, email: String!, password: String!, age: Int): User
+  createTodo(name: String!, isComplete: Boolean!, userId: ID!): Todo
+  deleteTodo(todoId: ID!): Todo
+  updateTodo(todoId: ID!, name: String, isComplete: Boolean): Todo
+  deleteTodos(todoIds: [ID!]): BatchPayload!
+  resetTodos: BatchPayload!
+}
+
+type User {
+  id: ID!
+  firstName: String!
+  email: String!
+  age: Int
+  todos(text: String): [Todo!]!
+}
+
+type Todo {
+  id: ID!
+  name: String!
+  isComplete: Boolean!
+  user: User!
+  userId: ID!
+}
+
+type Todos {
+  count: Int!
+  todoItems: [Todo!]
+}
+
+type BatchPayload {
+  count: Int!
+}
+
+`;
+
+module.exports = {
+  typeDefs,
+};
